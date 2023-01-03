236eb91b63fdc582d3c47ef180162bba8949c7fe
Natalia <raison-n@mail.ru>
2023-01-03 12:13:30 +0300

    Задача

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array1 = new string[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    string result = Convert.ToString(Console.ReadLine());
+    array1[i] = result;
+}
+Console.WriteLine();
+Console.Write("Введенный массив: [");
+Console.Write(string.Join(", ", array1));
+Console.Write("]");
+int count = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+Console.Write("Полученный массив: [");
+string[] array2 = new string[count];
+int j = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        array2[j] = array1[i];
+        Console.Write(array2[j] + "  ");
+        j++;
+    }
+}
+Console.Write("]");
